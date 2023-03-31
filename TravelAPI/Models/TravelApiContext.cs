664fd78b2bac731b07c49b59c4ba6195e33bde1a
Zachary <ZackCipolletta@gmail.com>
2023-03-31 08:52:07 -0700

    initial commit

---
+++ TravelAPI/Models/TravelApiContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ParksLookupApi.Models;
+
+namespace ParksLookupApi.Models
+{
+  public class ParksLookupApiContext : IdentityDbContext<ApplicationUser>
+  {
+    public DbSet<Destination> Destinations { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+
+    public ParksLookupApiContext(DbContextOptions<ParksLookupApiContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      // Configure your model here
+    }
+  }
+}
---
+++ TravelAPI/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ParksLookupApi.ViewModels
+{
+  public class RegisterViewModel
+  {
+      public string UserName { get; set; }
+    public string Password { get; set; }
+  }
+}
