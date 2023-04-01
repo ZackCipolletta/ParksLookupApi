a52a3d8cce0519165101b50798b9be1faa4704b2
Zachary <ZackCipolletta@gmail.com>
2023-03-31 18:49:39 -0700

    making 3rd attempt, created controller

--- ParksLookup/Program.cs
+++ ParksLookup/Program.cs
@@ -4,28 +4,23 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    .AddJwtBearer(options =>
     {
+      options.TokenValidationParameters = new TokenValidationParameters
+      {
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
         ValidateIssuer = true,
         ValidateAudience = true,
-        ValidAudience = "https://www.yogihosting.com",
-        ValidIssuer = "https://www.yogihosting.com",
-        ClockSkew = TimeSpan.Zero,// It forces tokens to expire exactly at token expiration time instead of 5 minutes later
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("SuperWeinerMan5000"))
-    };
-});
+        ValidateLifetime = false,
+        ValidateIssuerSigningKey = true
+      };
+    });
 
 builder.Services.AddControllers();
 
@@ -33,11 +28,10 @@ builder.Services.AddDbContext<ParksLookupApiContext>(
     dbContextOptions => dbContextOptions.UseMySql(
         builder.Configuration["ConnectionStrings:DefaultConnection"],
         ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"])
-    ) 
+    )
 );
 
-
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+builder.Services.AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<ParksLookupApiContext>()
     .AddDefaultTokenProviders();
 
@@ -48,15 +42,15 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 else
 {
-    app.UseHttpsRedirection();
+  app.UseHttpsRedirection();
 }
 
-app.UseAuthentication(); // Add authentication middleware before authorization middleware
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
